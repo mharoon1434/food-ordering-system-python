5a02fde11a0d12567f82dc10d52d25a7432026dd
Muhammad Haroon <m.haroon.4550@gmail.com>
2026-03-23 11:29:49 +0500

    main file

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+main.py
+Entry point of order management system.
+"""
+# Restored stored data form the file
+from RestoredModule import *
+# function on data 
+from FunctionModule import mainManu
+
+
+# entry function
+mainManu(Ad1,wrk,delivery,customer, food,orders)
